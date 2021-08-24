f5c31d2495c3f5cc9a60e6ecfc5b7f1ffe3ab98f
Willian Jean <willianjean@gmail.com>
2021-08-24 18:49:33 -0400

    First commit with retrofit added

---
+++ app/src/main/java/br/com/dio/app/repositories/App.kt
@@ -0,0 +1,20 @@
+package br.com.dio.app.repositories
+
+import android.app.Application
+import br.com.dio.app.repositories.data.di.DataModulo
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidContext(this@App)
+        }
+
+        DataModulo.load()
+
+    }
+}
